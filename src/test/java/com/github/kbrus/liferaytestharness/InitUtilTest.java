cb1d058af1c15455d04059da7f8e277a1c8f0d69
Konrad Brus <440355+KBrus@users.noreply.github.com>
2020-04-10 22:13:24 +0000

    Add portal startup tests.

---
+++ src/test/java/com/github/kbrus/liferaytestharness/InitUtilTest.java
@@ -0,0 +1,28 @@
+package com.github.kbrus.liferaytestharness;
+
+import com.liferay.counter.service.CounterLocalService;
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.util.InitUtil;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class InitUtilTest
+{
+	@Test
+	public void whenInit_thenCounterServiceWired()
+	{
+		InitUtil.init();
+
+		CounterLocalService service = CounterLocalServiceUtil.getService();
+		Assertions.assertThat(service).isNotNull();
+	}
+
+	@Test
+	public void whenInitWithSpring_thenCounterServiceWired()
+	{
+		InitUtil.initWithSpring();
+
+		CounterLocalService service = CounterLocalServiceUtil.getService();
+		Assertions.assertThat(service).isNotNull();
+	}
+}
