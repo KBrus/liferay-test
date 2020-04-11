500d14aa67484acfe675445fc155ce650c96ac52
Konrad Brus <440355+KBrus@users.noreply.github.com>
2020-04-11 18:45:04 +0200

    Add missing InitializationException.

---
+++ src/main/java/com/github/kbrus/liferaytestharness/InitializationException.java
@@ -0,0 +1,28 @@
+package com.github.kbrus.liferaytestharness;
+
+public class InitializationException extends Exception
+{
+	public InitializationException()
+	{
+	}
+
+	public InitializationException(String s)
+	{
+		super(s);
+	}
+
+	public InitializationException(String s, Throwable throwable)
+	{
+		super(s, throwable);
+	}
+
+	public InitializationException(Throwable throwable)
+	{
+		super(throwable);
+	}
+
+	public InitializationException(String s, Throwable throwable, boolean b, boolean b1)
+	{
+		super(s, throwable, b, b1);
+	}
+}
